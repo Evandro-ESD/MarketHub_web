7f24b7f78f8248c9fcd1cf3b3dc73427c8fa01ff
Evandro dos Santos Dias <vando86206@gmail.com>
2025-08-27 16:00:20 -0300

    botão genérico para rotas no header

--- markethub_web/src/app/shared/components/button/button.component.ts
+++ markethub_web/src/app/shared/components/button/button.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+
+  @Input() text: string = 'Botão';
+  @Input() color: 'primary' | 'secondary' | 'danger' = 'primary';
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick() {
+    this.clicked.emit();
+  }
 
 }
