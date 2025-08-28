26c2e7c98b7162ffeb9b21dc4366eefa47dde9c0
Evandro dos Santos Dias <vando86206@gmail.com>
2025-08-28 15:13:05 -0300

    form cadastro

--- markethub_web/src/app/features/auth/cadastro/cadastro.component.ts
+++ markethub_web/src/app/features/auth/cadastro/cadastro.component.ts
@@ -1,32 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators, } from "@angular/forms";
+import { FormBuilder, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from "@angular/forms";
 
 @Component({
   selector: 'app-cadastro',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+
+  loading = false;
 
   fb = inject(FormBuilder)
 
   formCadastro = this.fb.group({
     nome: ['', [Validators.required]],
     senha: ['', [Validators.required]],
-    image: ['']
+    foto: [''],
+    perfil: ['']
   })
-
-  loading = false;
 
   cadastrar() {
     this.loading = true;
 
+    // console.log(this.formCadastro.value)
+
+
+
     // Simulação de envio
     setTimeout(() => {
       this.loading = false;
-      alert('Usuário cadastrado com sucesso!');
+      alert('Usuário cadastrado com sucesso!\n');
       this.limparFormulario();
     }, 2000); // aqui você chamaria seu serviço de cadastro real
   }
