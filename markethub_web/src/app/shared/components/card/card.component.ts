3963f2abfc089d12443ca9999d568913cf0f87cd
Bruno Groppo <bmgroppo@gmail.com>
2025-09-01 10:40:15 -0300

    Feito @for de produtos

--- markethub_web/src/app/shared/components/card/card.component.ts
+++ markethub_web/src/app/shared/components/card/card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  // Entradas: Dados que o componente recebe do pai
+  @Input() imageUrl: string = 'https://picsum.photos/300/200'; // Imagem padrão
+  @Input() titulo: string = 'Nome do Produto';
+  @Input() descricao: string = 'Descrição do produto aqui.';
+  @Input() preco: number = 0;
 
+  // Saída: Evento que o componente emite para o pai
+  @Output() adicionarAoCarrinho = new EventEmitter<void>();
 
+  aoAdicionarAoCarrinho(): void {
+    // Quando o botão é clicado, emitimos o evento.
+    this.adicionarAoCarrinho.emit();
+  }
 }
