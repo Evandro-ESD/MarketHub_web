f7ba001836ac287c4402d0244ce026888d818a09
Evandro dos Santos Dias <vando86206@gmail.com>
2025-09-02 09:59:56 -0300

    --

--- markethub_web/src/app/features/admin/dashboard/dashboard.component.ts
+++ markethub_web/src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import { Component, inject, OnInit } from '@angular/core';
-import { CardComponent } from "../../../shared/components/card/card.component";
-import { ProdutoService } from '../../../core/services/produtos.service';
-import { Produtos } from '../../../shared/entities/produtos.entity';
-
-@Component({
-  selector: 'app-dashboard',
-  imports: [CardComponent],
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'] // corrigido: styleUrls (plural)
-})
-export class DashboardComponent implements OnInit {
-  private produtoService = inject(ProdutoService);
-  listaProdutos: Produtos[] = []; // lista tipada
-
-  ngOnInit(): void {
-    this.carregarProdutos();
-  }
-
-  carregarProdutos() {
-    this.produtoService.getProdutos().subscribe({
-      next: (produtos) => {
-        this.listaProdutos = produtos;
-        console.log('Produtos carregados:', produtos);
-      },
-      error: (err) => {
-        console.error('Erro ao carregar produtos:', err);
-      }
-=======
 import { DecimalPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
@@ -62,7 +32,6 @@ export class DashboardComponent implements OnInit {
     // Subscreve ao BehaviorSubject para atualizar automaticamente
     this.authService.currentUser$.subscribe((u) => {
       this.user = u;
->>>>>>> 44a3831138e1eed025525557944b271f723d9d0d
     });
   }
 }
