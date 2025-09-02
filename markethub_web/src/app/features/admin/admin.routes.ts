57f932a706c3147f222ce68460e8373d9e79bbb3
Fabio <fabioluizrl2@gmail.com>
2025-09-02 13:10:20 -0300

    adicionado dashboard use npm install na raiz pra isnatar chart

--- markethub_web/src/app/features/admin/admin.routes.ts
+++ markethub_web/src/app/features/admin/admin.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RelatoriosComponent } from './relatorios/relatorios.component';
 import { TelaProdutosComponent } from './tela-produtos/tela-produtos.component';
 
 export const adminRoutes: Routes = [
   { path: 'tela-produtos', component: TelaProdutosComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'relatorio', component: RelatoriosComponent}
   // { path: 'tela-produtos', component: TelaProdutosComponent, canActivate:[AdminGuard] },
-  // { path: 'dashboard', component: DashboardComponent, canActivate:[AdminGuard] },
   // { path: 'relatorio', component: RelatoriosComponent, canActivate:[AdminGuard]},
 ];
