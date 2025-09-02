30fc03f1d7891d783ed2514667a5928a5fd90917
Fabio <fabioluizrl2@gmail.com>
2025-09-02 13:56:35 -0300

    adicionado tema header

--- markethub_web/src/app/features/home/header/header.component.ts
+++ markethub_web/src/app/features/home/header/header.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { LinkComponent } from "../../../shared/components/link/link.component";
 import { User } from './../../../shared/entities/user.entity';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'] // se tiver estilos
-  ,
-  imports: [LinkComponent]
+  styleUrls: ['./header.component.css'],
+  imports: [LinkComponent, NgIf, NgFor]
 })
 export class HeaderComponent implements OnInit {
   user?: User | null;
+  menuOpen = signal(false);
+  toggleMenu(){ this.menuOpen.set(!this.menuOpen()); }
 
   constructor(private authService: AuthService) {}
 
