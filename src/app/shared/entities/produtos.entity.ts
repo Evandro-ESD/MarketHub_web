6f4615dfafc83df2f360cecab25788c282b16006
Evandro dos Santos Dias <vando86206@gmail.com>
2025-09-03 12:23:18 -0300

    ajuste merge

--- src/app/shared/entities/produtos.entity.ts
+++
@@ -1,9 +0,0 @@
-export interface Produtos {
-    id_produto: number
-    nome_produto: string
-    descricao: string
-    foto?: File | string;
-    preco: number
-    estoque: number
-    id_vendedor: number
-}
